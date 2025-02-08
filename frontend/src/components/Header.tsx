158449ada13d03bde632c664ff4fe93de52a81f1
quantinium03 <vaibhavvx331@gmail.com>
2025-02-08 16:55:15 +0530

    header github

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,22 +1,23 @@
-import { Download, GithubLogo } from "@phosphor-icons/react"
+import { GithubLogo } from "@phosphor-icons/react";
 
 const Header = () => {
-  return (
-    <div className="flex justify-between border-b-[#d79921] border py-2 px-3">
-      <div>
-        <span className="text-xl md:text-3xl font-bold text-[#d79921]">LaTed</span>
-      </div>
-      <div className="flex gap-2 md:gap-3">
-        <button className="border rounded-md aspect-square self-center p-1 text-[#d79921] font-bold">
-          <Download size={20} className="md:w-6 md:h-6" />
-        </button>
-        <button className="border rounded-md aspect-square self-center p-1 text-[#d79921] font-bold">
-          <GithubLogo size={20} className="md:w-6 md:h-6" />
-        </button>
-      </div>
-    </div>
-  )
-}
+    return (
+        <div className="flex justify-between border-b border-[#d79921] py-2 px-3">
+            <div>
+                <span className="text-xl md:text-3xl font-bold text-[#d79921]">LaTed</span>
+            </div>
+            <div>
+                <a
+                    href="https://github.com/quantinium03/lated"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="border rounded-md aspect-square flex items-center justify-center p-2 text-[#d79921] hover:bg-[#d79921] hover:text-white transition"
+                >
+                    <GithubLogo size={20} className="md:w-6 md:h-6" />
+                </a>
+            </div>
+        </div>
+    );
+};
 
-export default Header
-
+export default Header;
